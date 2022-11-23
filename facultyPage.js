c605e0279de066df233e4a3b9817f9e2dccd2269
shingggk <kiseshingo@cityuniversity.edu>
2022-11-22 17:31:03 -0800

    DELETE API & Refresh Button
    add Refresh button to fetch DELETE API to delete items from a table

--- facultyPage.js
+++ facultyPage.js
@@ -1,22 +1,30 @@
 "use strict";
 const getURL =
   "https://ilcct0rpcf.execute-api.us-west-2.amazonaws.com/get_v1/getstudentdata";
+
+const deleteURL =
+  "https://ilcct0rpcf.execute-api.us-west-2.amazonaws.com/delete_v1/deletestudentdata?id=";
+
 const participants = document.getElementById("studentList");
 const studentTotal = document.getElementById("studentsTotal");
+const refreshBtn = document.querySelector(".refresh-btn");
 
 async function fetchStudentsList() {
   try {
     const resStudent = await fetch(getURL);
     const dataStudent = await resStudent.json();
     const items = dataStudent.Items;
-    studentTotal.innerHTML = `Signed-in Students: ${items.length}`;
+    console.log(items);
     await displayData(items);
+    await deleteData(items);
   } catch {
     console.error("Error while fetching:");
   }
 }
 
 async function displayData(items) {
+  studentTotal.innerHTML = `Signed-in Students: ${items.length}`;
+
   let names = [];
   for (let i = 0; i < items.length; i++) {
     names.push([
@@ -37,6 +45,16 @@ async function displayData(items) {
       </tr>`
     );
   }
+}
+
+async function deleteData(items) {
+  refreshBtn.addEventListener("click", function () {
+    let ids = items.map((item) => item.id.S);
+    for (let id of ids) {
+      fetch(deleteURL + id, { method: "DELETE" });
+    }
+    console.log("clicked!");
+  });
 }
 
 fetchStudentsList();
